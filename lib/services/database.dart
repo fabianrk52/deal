f1428be50d55c3902ea6972eae89488d1ff5a08c
Fabian <fabianrk@gmail.com>
2021-09-22 17:42:14 -0300

    fixing db init

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
   final String? uid;
-  DatabaseService(uid, {this.uid});
+  DatabaseService({this.uid});
 
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection("users");
